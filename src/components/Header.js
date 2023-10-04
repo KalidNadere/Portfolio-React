a22457b6b7565e6998015632cf4008d3bcdca416
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-10-04 21:46:46 +1100

    installed dependecies, updated header.js to import necessary files and define header function

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+// Importing react library
+import React from 'react';
+// Importing navigation from local file
+import Navigation from './Navigation';
+
+// 
+function Header() {
+  return (
+    <Header>
+      <h1>Kalid Nadere</h1>
+      <Navigation />
+    </Header>
+  );
+}
+
+export default Header;
