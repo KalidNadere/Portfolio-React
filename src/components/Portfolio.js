e6616945800966c0143b093454910676a3d9ceea
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-10-06 19:44:02 +1100

    added code for portfolio.js to add different project, used 'map' for rendering project components

---
+++ src/components/Portfolio.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Project from './Project';
+
+// Portfolio function defined
+function Portfolio() {
+  // My projects data
+  const projects = [
+    // Array of project objects, each containing properties
+    {
+      title: 'MovieBuff',
+      image: 'project1.jpg',
+      description: 'Movie Buff is a search engine for movies....',
+      siteLink: 'https://bemonn.github.io/Movie-buff/',
+      githubLink: 'https://github.com/KalidNadere/Movie-buff',
+    },
+    // More projects to be added..
+  ];
+
+  // JSX structure, including 'h2' & 'div' elements
+  return (
+    <section>
+      <h2>Portfolio</h2>
+      <div className='project-list'>
+        {/* using 'map' function to iterate over projects array and render a project component for each project in the list */}
+        {projects.map((project, index) => (
+          <Project key={index} project={project} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Portfolio;
